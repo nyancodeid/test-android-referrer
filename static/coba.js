be52728c1e592c04ab3833b50fce7ca17ff97513
nyancodeid <ryandevstudio@gmail.com>
2018-08-28 13:23:36 +0700

    push

--- static/coba.js
+++ static/coba.js
@@ -1,15 +1,25 @@
+String.prototype.isEmpty = function() {
+    return (this.length === 0 || !this.trim());
+};
+
 var Events = {
     onDeviceReady: function() {
 		var prefs = plugins.appPreferences;
 		var referral = null;
 		var count = 0;
 		var intervalReferrer = setInterval(function() {
-			document.getElementById('nyan').innerHTML = "Loading";
+			document.getElementById('nyan').innerHTML = "Loading..";
 
 			if (referral == null) {
 				if (count < 5) {
 					prefs.fetch(function(value) { 
-						referral = value;			
+						if (!value.toString().isEmpty()) {
+							referral = value;
+
+							if (referral == "NyanDev" || referral == "nyandev") {
+								alert('Done, test sukses.');
+							}
+						}	
 					}, function(error) {
 						alert('Error: ' + error)
 					}, 'referrer');
